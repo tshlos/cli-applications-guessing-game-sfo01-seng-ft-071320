9bc7570f55ce097949583929aac43897faadef73
Taci Shlosberg <tshlosberg@gmail.com>
2020-07-07 19:55:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,26 @@
 # Code your solution here!
+require 'pry'
+
+def run_guessing_game
+  
+  random_num = rand(6) + 1
+  input_str = gets.chomp
+  binding.pry
+  input_num = gets.to_i
+  binding.pry
+  
+  
+
+  if input == random_num
+    puts "You guessed the correct number!"
+    return
+  elsif input != random_num
+  binding.pry
+    puts "Sorry! The computer guessed #{random_num}."
+    return
+  else
+    puts "Goodbye!"
+  end
+    binding.pry
+  
+end
