3e4f4715a37ac80d678c223d02456dd1a4d9acef
Taci Shlosberg <tshlosberg@gmail.com>
2020-07-07 19:56:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,16 +5,14 @@ def run_guessing_game
   
   random_num = rand(6) + 1
   input_str = gets.chomp
-  binding.pry
   input_num = gets.to_i
-  binding.pry
-  
+
   
 
-  if input == random_num
+  if input_num == random_num
     puts "You guessed the correct number!"
     return
-  elsif input != random_num
+  elsif input_num != random_num
   binding.pry
     puts "Sorry! The computer guessed #{random_num}."
     return
